f76db9d48e4da691d3351d05e9a50b62b02f829b
Amol C <amolch001@gmail.com>
2025-01-09 10:21:54 +0530

    added the proper prod.sh

--- prod.sh
+++ prod.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 cp service.server.js  admin/angular/service.js
 cp service.server.js  public/angular/service.js
-
-node freshpicks.js
-
 npm install
-
 echo "killing the freshpicks.js"
 ps -ef | grep "node freshpicks.js" | grep -v runstatic | awk '{print $2}' | xargs kill -9
 echo "Restarting Service"
