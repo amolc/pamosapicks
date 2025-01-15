ddf439e503d47af3edc9880168fee506b4e838c4
Lusayo Nyondo <lusayohendy@gmail.com>
2025-01-15 09:09:01 +0200

    Added pages for orders/thankyou/view.

---
+++ public/angular/ordersCtrl.js
@@ -0,0 +1,82 @@
+app.controller('ordersCtrl', function ($scope, $http, $window, $location, $sce, $timeout, store, config) {
+    $scope.data = {};
+    $scope.dataset = [];
+
+    $scope.list = function () {
+      $http.get(`${config.baseurl}orders/order/`)
+        .then(function (response) {
+            if (response.data.status === 'false') {
+                console.error("Error fetching orders list:", response.data.message);
+            } else {
+                $scope.dataset = response.data.data;
+                console.log("Order list fetched:", $scope.dataset);
+            }
+        })
+        .catch(function (error) {
+            console.error("Error fetching orders list:", error);
+        });
+    };
+
+    // Fetch a single order by ID
+    $scope.getOrderbyid = function (id) {
+        if (!order_id) {
+            console.error("Order ID is missing!");
+            return;
+        }
+    
+        $http.get(`${config.baseurl}orders/get-order/${order_id}/`)
+            .then(function (response) {
+                if (response.data.status === 'false') {
+                    console.error("Error fetching order:", response.data.message);
+                } else {
+                    if (!response.data.data) {
+                        console.error("Order not found for ID:", order_id);
+                        $scope.order = {}; // Empty category data if not found
+                    } else {
+                        // Ensure price and discount_price are numbers if they exist
+                        $scope.order = response.data.data;
+                        $scope.order.price = parseFloat($scope.order.price) || 0;  // Ensure price is a number
+                        $scope.order.discount_price = parseFloat($scope.order.discount_price) || null;  // Ensure discount_price is a number
+    
+                        console.log("Order fetched:", $scope.order);
+                    }
+                }
+            })
+            .catch(function (error) {
+                console.error("Error fetching order:", error);
+            });
+    };
+    
+    $scope.init = function () {
+        $scope.list();
+    }; 
+    
+    $scope.ondelete = function (data) {
+        console.log("Delete modal triggered with data:", data);
+        $scope.order = angular.copy(data); // Bind the order data to $scope.order
+        $("#deleteform").modal("show"); // Show the delete confirmation modal
+    };
+
+    // Open the edit modal
+    $scope.onview = function (data) {
+        $scope.order = angular.copy(data); // Copy data to prevent binding issues
+        $("#viewform").modal("show");
+    };
+
+    // Close modal manually
+    $scope.closeModal = function () {
+        $("#deleteform").modal("hide");
+    };
+
+    $scope.closeeditModal = () => {
+        $("#editform").modal("hide");
+    };
+
+    $scope.closeaddModal = function () {
+        $("#addform").modal("hide");
+    };
+});
+    
+function propertyNameFromModelPath(modelPath) {
+    return modelPath.split('.').pop();
+}
---
+++ public/angular/viewOrderCtrl.js
@@ -0,0 +1,73 @@
+app.controller('viewOrderCtrl', function ($scope, $http, $window, $location, $sce, $timeout, store, config) {
+    $scope.order = {};
+    $scope.newOrderStatus = '';
+
+    $scope.urlParams = Object.fromEntries(
+        new URLSearchParams(window.location.search)
+    );
+
+    $scope.getOrder = function () {
+        $scope.fetchingOrder = true;
+
+        let url = '';
+
+        if($scope.urlParams.length == 0) {
+            console.error("No ID specified.");
+            return;
+        }
+
+        if(!$scope.urlParams.hasOwnProperty('id')) {
+            console.error("No ID specified.");
+            return;
+        }
+
+        url = `${config.baseurl}orders/get-order/${$scope.urlParams['id']}`;
+        
+        $http.get(url)
+            .then(function (response) {
+                if (response.data.status === 'error') {
+                    console.error("Error fetching order:", response.data.message);
+                } else {
+                    $scope.order = response.data.data;
+                    $scope.newOrderStatus = $scope.order.status;
+                }
+            })
+            .catch(function (error) {
+                console.error("Error fetching orders list:", error);
+            });
+    };
+    
+    $scope.showChangeOrderStatusModal = () => {
+        $("#changeOrderStatusModal").modal('show');
+    };
+
+    $scope.showStatusChangeHistoryModal = () => {
+        $("#statusChangeHistoryModal").modal('show');
+    };
+
+    $scope.submitOrderStatusChange = () => {
+        const id = $scope.order.id;
+        let url = `${config.baseurl}orders/change-order-status/${$scope.urlParams['id']}`;
+        let data = JSON.stringify({
+            'status': $scope.newOrderStatus
+        });
+
+        $http.post(url, data).then(response => {
+            if (response.data.status === 'error') {
+                console.error("Error fetching order:", response.data.message);
+            } else {
+                window.location.reload();
+            }
+        }).error(error => {
+            console.error("Error fetching orders list:", error);
+        });
+    }
+
+    $scope.init = function () {
+        $scope.getOrder();
+    }; 
+});
+    
+function propertyNameFromModelPath(modelPath) {
+    return modelPath.split('.').pop();
+}
